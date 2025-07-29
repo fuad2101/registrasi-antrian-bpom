96e1f839d14b6291ed383e9b855c8793a673686f
Fuad <muhfuad67@gmail.com>
2025-07-30 05:22:06 +0800

    - Created index.blade.php for the main page layout , Added CSS styles in style.css , Updated web.php to return the new index view on the root route.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+
+<html lang="zxx">
+
+<head>
+  <meta charset="utf-8">
+  <title>Antrian Layanan | BPOM Mamuju</title>
+
+  <!-- mobile responsive meta -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+  <!-- theme meta -->
+  <meta name="theme-name" content="dtox" />
+
+  <!-- ** Plugins Needed for the Project ** -->
+  <!-- Bootstrap -->
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+<link rel="stylesheet" href="plugins/bootstrap/bootstrap.min.css">
+
+  <!-- Main Stylesheet -->
+  <link href="css/style.css" rel="stylesheet">
+
+  <!--Favicon-->
+  <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+  <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+
+</head>
+
+<body>
+
+
+<!-- hero area -->
+<section class="hero-section hero" data-background="" style="background-image: url(images/hero-area/banner-bg.png);">
+  <div class="container">
+    <div class="row mt-3">
+      <div class="col-2">
+        <img class="img-fluid" src="{{asset('storage/images/logo.png')}}" alt="">
+      </div>
+      <div class="col-10 mt-4 text-center">
+        <h2 class="mb-1 ">Registrasi Antrian BPOM Mamuju</h2>
+        <h4 class=" mb-4">Silahkan login atau daftar untuk mengambil antrian</h4>
+      </div>
+    </div>
+  </div>
+  <div class="scroll-left mt-5">
+    <p class="fw-bold">Follow Sosial media kami : Instagram : @bpom.mamuju X:@bpommamuju Facebook:@bpom.mamuju YouTube:@balaipomdimamuju</p>
+  </div>
+  <!-- background shapes -->
+  <!-- <div id="scene">
+    <img class="img-fluid hero-bg-1 up-down-animation" src="images/background-shape/feature-bg-2.png" alt="">
+    <img class="img-fluid hero-bg-2 left-right-animation" src="images/background-shape/seo-ball-1.png" alt="">
+    <img class="img-fluid hero-bg-3 left-right-animation" src="images/background-shape/seo-half-cycle.png" alt="">
+    <img class="img-fluid hero-bg-4 up-down-animation" src="images/background-shape/green-dot.png" alt="">
+    <img class="img-fluid hero-bg-5 left-right-animation" src="images/background-shape/blue-half-cycle.png" alt="">
+    <img class="img-fluid hero-bg-6 up-down-animation" src="images/background-shape/seo-ball-1.png" alt="">
+    <img class="img-fluid hero-bg-7 left-right-animation" src="images/background-shape/yellow-triangle.png" alt="">
+    <img class="img-fluid hero-bg-8 up-down-animation" src="images/background-shape/service-half-cycle.png" alt="">
+    <img class="img-fluid hero-bg-9 up-down-animation" src="images/background-shape/team-bg-triangle.png" alt="">
+  </div> -->
+</section>
+<!-- /hero-area -->
+
+<!-- pricing -->
+<section class="section-lg pricing" id="pricing">
+  <div class="container">
+    <div class="row">
+      <!-- <div class="col">
+        <img src="https://placehold.co/600x700" alt="">
+      </div>
+      <div class="col">
+        <div class="row d-flex flex-column justify-content-around">
+          <div class="col-md-6 col-lg-12 mb-5 mb-lg-3 mt-5 pt-4">
+            <div class="rounded text-center pricing-table table-1">
+              <h4>Informasi & Konsultasi</h4>
+              <h1 id="loketA">A-000</h1>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-12 mb-5 mb-lg-3">
+            <div class="rounded text-center pricing-table table-2">
+              <h4>Pengaduan</h4>
+              <h1 id="loketB">B-000</h1>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div> -->
+    </div>
+  </div>
+  <div class="row">
+    <div class="col ml-5">
+        <img src="{{asset('storage/images/maskot.png')}}" class="d-block w-75" alt="https://placehold.co/900x700">
+      {{-- <div id="sliderFoto" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="{{asset('storage/images/maskot.png')}}" class="ml-5 d-block w-75" alt="https://placehold.co/900x700">
+          </div>
+          <div class="carousel-item">
+            <img src="images\slideshow\1.png" class="d-block w-100 img-thumbnail" alt="https://placehold.co/900x700">
+            <!-- <div class="carousel-caption d-none d-md-block">
+              <h5>First slide label</h5>
+              <p>Some representative placeholder content for the first slide.</p>
+            </div> -->
+          </div>
+          <div class="carousel-item">
+            <img src="images\slideshow\standar-layanan\1.png" class="d-block w-100 img-thumbnail" alt="https://placehold.co/900x700">
+          </div>
+          <div class="carousel-item">
+            <img src="images\slideshow\standar-layanan\3.png" class="d-block w-100 img-thumbnail" alt="https://placehold.co/900x700">
+          </div>
+          <div class="carousel-item">
+            <img src="images\slideshow\standar-layanan\4.png" class="d-block w-100 img-thumbnail" alt="https://placehold.co/900x700">
+          </div>
+          <div class="carousel-item">
+            <img src="images\slideshow\standar-layanan\5.png" class="d-block w-100 img-thumbnail" alt="https://placehold.co/900x700">
+          </div>
+          <div class="carousel-item">
+            <img src="images\slideshow\standar-layanan\6.png" class="d-block w-100 img-thumbnail" alt="https://placehold.co/900x700">
+          </div>
+          <div class="carousel-item">
+            <img src="images\slideshow\standar-layanan\7.png" class="d-block w-100 img-thumbnail" alt="https://placehold.co/900x700">
+          </div>
+          <div class="carousel-item">
+            <img src="images\slideshow\standar-layanan\8.png" class="d-block w-100 img-thumbnail" alt="https://placehold.co/900x700">
+          </div>
+          <div class="carousel-item">
+            <img src="images\slideshow\standar-layanan\9.png" class="d-block w-100 img-thumbnail" alt="https://placehold.co/900x700">
+          </div>
+          <div class="carousel-item">
+            <img src="images\slideshow\standar-layanan\10.png" class="d-block w-100 img-thumbnail" alt="https://placehold.co/900x700">
+          </div>
+          <div class="carousel-item">
+            <img src="images\slideshow\standar-layanan\11.png" class="d-block w-100 img-thumbnail" alt="https://placehold.co/900x700">
+          </div>
+          <div class="carousel-item">
+            <img src="images\slideshow\standar-layanan\12.png" class="d-block w-100 img-thumbnail" alt="https://placehold.co/900x700">
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#sliderFoto" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#sliderFoto" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div> --}}
+    </div>
+    <div class="col mr-5">
+      <div class="row d-flex flex-column justify-content-around">
+        <!-- pricing table -->
+        <div class="col-md-6 col-lg-12 mb-5 mb-lg-3 pt-4">
+          <div class="rounded text-center pricing-table table-1">
+            <form action="" method="get">
+                <button type="submit" class="btn btn-lg btn-primary">Login</button>
+            </form>
+            <h4>Login</h4>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-12 mb-5 mb-lg-3">
+          <div class="rounded text-center pricing-table table-2">
+            <h4>Register</h4>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- background shapes -->
+  {{-- <img class="pricing-bg-shape-1 up-down-animation" src="images/background-shape/seo-ball-1.png" alt="background-shape">
+  <img class="pricing-bg-shape-2 up-down-animation" src="images/background-shape/seo-half-cycle.png" alt="background-shape">
+  <img class="pricing-bg-shape-3 left-right-animation" src="images/background-shape/team-bg-triangle.png" alt="background-shape"> --}}
+</section>
+<!-- /pricing -->
+
+<!-- Marquee -->
+
+
+<!-- footer -->
+<footer class="footer-section footer" style="background-image: url(images/backgrounds/footer-bg.png);">
+  <div class="container">
+    <div class="row">
+      <div class="col text-center text-lg-left mb-lg-0">
+        <!-- <a href="index.html">
+          <img class="img-fluid" src="images/logo.png" alt="logo">
+        </a> -->
+        <p class="text-center"> Copyright &copy BPOM Mamuju 2025</p>
+      </div>
+      <!-- <nav class="col-lg-8 align-self-center mb-5">
+        <ul class="list-inline text-lg-right text-center footer-menu">
+          <li class="list-inline-item active"><a href="index.html">Home</a></li>
+          <li class="list-inline-item"><a class="page-scroll" href="#feature">Feature</a></li>
+          <li class="list-inline-item"><a href="about.html">About</a></li>
+          <li class="list-inline-item"><a class="page-scroll" href="#team">Team</a></li>
+          <li class="list-inline-item"><a class="page-scroll" href="#pricing">Pricing</a></li>
+          <li class="list-inline-item"><a href="contact.html">Contact</a></li>
+        </ul>
+      </nav>
+      <nav class="col-12">
+        <ul class="list-inline text-lg-right text-center social-icon">
+          <li class="list-inline-item">
+            <a class="facebook" href="#"><i class="ti-facebook"></i></a>
+          </li>
+          <li class="list-inline-item">
+            <a class="twitter" href="#"><i class="ti-twitter-alt"></i></a>
+          </li>
+          <li class="list-inline-item">
+            <a class="linkedin" href="#"><i class="ti-linkedin"></i></a>
+          </li>
+          <li class="list-inline-item">
+            <a class="black" href="#"><i class="ti-github"></i></a>
+          </li>
+        </ul>
+      </nav> -->
+
+    </div>
+  </div>
+
+</footer>
+<!-- /footer -->
+
+<!-- jQuery -->
+<script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+<!-- Bootstrap JS -->
+<script src="plugins/bootstrap/bootstrap.min.js"></script>
+<!-- slick slider -->
+<script src="plugins/slick/slick.min.js"></script>
+<!-- venobox -->
+<script src="plugins/Venobox/venobox.min.js"></script>
+<!-- aos -->
+<script src="plugins/aos/aos.js"></script>
+<!-- Main Script -->
+<script src="js/script.js"></script>
+
+
+</body>
+</html>
